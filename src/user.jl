9ade413b70af15650760321d54205fcaf8b7c594
abieler <andre.bieler81@gmail.com>
2015-11-25 23:09:52 -0500

    mods

--- src/user.jl
+++ src/user.jl
@@ -3,17 +3,17 @@ using Types
 mySettings = UserSettings()
 body = MeshBody
 
-mySettings.nIterations = 100
+mySettings.nIterations = 30
 mySettings.meshFileName = "../input/sphere2.ply"
 
-mySettings.domainSizeX = 5000.0
-mySettings.domainSizeY = 5000.0
-mySettings.domainSizeZ = 5000.0
+mySettings.domainSizeX = 50000.0
+mySettings.domainSizeY = 50000.0
+mySettings.domainSizeZ = 50000.0
 
 mySettings.nCellsPerBlockX = 5
 mySettings.nCellsPerBlockY = 5
 mySettings.nCellsPerBlockZ = 5
 
-mySettings.nMaxRefinementLevel = 5
+mySettings.nMaxRefinementLevel = 11
 mySettings.nMaxTrianglesPerCell = 1
-mySettings.nNewParticlesPerIteration = 1000
+mySettings.nNewParticlesPerIteration = 4000000
